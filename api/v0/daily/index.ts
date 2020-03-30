b855c5328667da2600b5e3532bad990ce5547422
Alan Campora <alan.campora@gmail.com>
2020-03-30 14:17:31 -0300

    refactor(*): data formatted

--- api/v0/daily/index.ts
+++ api/v0/daily/index.ts
@@ -5,47 +5,74 @@ import R from 'ramda';
 const URL =
 	'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Argentina';
 
-//async function main(request: NowRequest, response: NowResponse) {
-//const browser = await puppeteer.launch({
-//args: chromium.args,
-//executablePath: await chromium.executablePath,
-//headless: false,
-//});
-
-//const page = await browser.newPage();
-//await page.goto(URL);
-//await page.evaluate(() => {
-//const table = document.querySelector(
-//'.wikitable.mw-collapsible.mw-made-collapsible',
-//);
-//console.log({ table });
-//});
-
-//const { name = 'World' } = request.query;
-//response.status(200).send(`Hello ${name}!`);
-//}
-
 const hasNumber = x => x.match(/\d+\s+/g);
 
-async function newMain(request: NowRequest, response: NowResponse) {
-	// Promise interface
+const countriesFormatter = {
+	Date: 'fecha',
+	'City of Buenos Aires': 'caba',
+	'Buenos Aires Province': 'buenos_aires',
+	Chaco: 'cacho',
+	Córdoba: 'cordoba',
+	Corrientes: 'corrientes',
+	'Entre Ríos': 'entre_rios',
+	Jujuy: 'jujuy',
+	'La Pampa': 'la_pampa',
+	Mendoza: 'mendoza',
+	Misiones: 'misiones',
+	Neuquén: 'neuquen',
+	'Río Negro': 'rio_negro',
+	Salta: 'salta',
+	'San Juan': 'san_juan',
+	'San Luis': 'san_luis',
+	'Santa Cruz': 'santa_cruz',
+	'Santa Fe': 'santa_fe',
+	'Santiago del Estero': 'sgo_del_stero',
+	'Tierra del Fuego': 'tierra_del_fuego',
+	Tucumán: 'tucuman',
+	'Total infections[a]': 'total_infections',
+	'Total deaths': 'total_deaths',
+	'New cases[a]': 'new_cases',
+	'New deaths': 'new_deaths',
+};
+
+async function main(request: NowRequest, response: NowResponse) {
 	const { data } = await scrape(URL, {
 		table: {
 			selector: '.wikitable.mw-collapsible tbody tr th',
 			convert: x => {
 				const data = x.split('\n');
 
-				const columns = data.filter(x => !hasNumber(x));
+				let columns = data.filter(x => !hasNumber(x));
 
 				const rows = data.filter(hasNumber);
+
+				console.log({ columns, rows });
+
+				columns = columns.map(c => countriesFormatter[c]);
 
 				return [columns, rows];
 			},
 		},
 		content: {
 			listItem: '.wikitable.mw-collapsible tbody tr td',
+			convert: x => {
+				const replaceObject = x => (typeof x === 'object' ? '0' : x);
+				const replaceNumbersInParents = x => x.replace(/\s*\([0-9]*\)/g, '');
+				const replaceNumbersInBrackets = x => x.replace(/\s*\[.*\]/g, '');
+				const replaceNull = x => x.replace(/—/g, 0);
+				const formatNumber = R.compose(
+					Number,
+					replaceNull,
+					replaceNumbersInParents,
+					replaceNumbersInBrackets,
+					replaceObject,
+				);
+
+				const formatted = formatNumber(x);
+
+				return formatted;
+			},
 		},
-		title: '.firstHeading',
 	});
 
 	data.content = R.splitEvery(24, data.content);
@@ -72,4 +99,4 @@ async function newMain(request: NowRequest, response: NowResponse) {
 	response.status(200).send(formattedData);
 }
 
-export default newMain;
+export default main;
