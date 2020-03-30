4a779e1c9bb961436efaa2324561a34936a9f58e
Alan Campora <alan.campora@gmail.com>
2020-03-30 17:09:51 -0300

    refactor(daily): format date

--- api/v0/daily/index.ts
+++ api/v0/daily/index.ts
@@ -7,32 +7,64 @@ const URL =
 
 const hasNumber = x => x.match(/\d+\s+/g);
 
-const provincesFormatter = {
-	Date: 'fecha',
-	'City of Buenos Aires': 'caba',
-	'Buenos Aires Province': 'buenos_aires',
-	Chaco: 'chaco',
-	Córdoba: 'cordoba',
-	Corrientes: 'corrientes',
-	'Entre Ríos': 'entre_rios',
-	Jujuy: 'jujuy',
-	'La Pampa': 'la_pampa',
-	Mendoza: 'mendoza',
-	Misiones: 'misiones',
-	Neuquén: 'neuquen',
-	'Río Negro': 'rio_negro',
-	Salta: 'salta',
-	'San Juan': 'san_juan',
-	'San Luis': 'san_luis',
-	'Santa Cruz': 'santa_cruz',
-	'Santa Fe': 'santa_fe',
-	'Santiago del Estero': 'sgo_del_stero',
-	'Tierra del Fuego': 'tierra_del_fuego',
-	Tucumán: 'tucuman',
-	'Total infections[a]': 'total_infections',
-	'Total deaths': 'total_deaths',
-	'New cases[a]': 'new_cases',
-	'New deaths': 'new_deaths',
+const formatProvince = province => {
+	const provincesFormatter = {
+		Date: 'fecha',
+		'City of Buenos Aires': 'caba',
+		'Buenos Aires Province': 'buenos_aires',
+		Chaco: 'chaco',
+		Córdoba: 'cordoba',
+		Corrientes: 'corrientes',
+		'Entre Ríos': 'entre_rios',
+		Jujuy: 'jujuy',
+		'La Pampa': 'la_pampa',
+		Mendoza: 'mendoza',
+		Misiones: 'misiones',
+		Neuquén: 'neuquen',
+		'Río Negro': 'rio_negro',
+		Salta: 'salta',
+		'San Juan': 'san_juan',
+		'San Luis': 'san_luis',
+		'Santa Cruz': 'santa_cruz',
+		'Santa Fe': 'santa_fe',
+		'Santiago del Estero': 'sgo_del_stero',
+		'Tierra del Fuego': 'tierra_del_fuego',
+		Tucumán: 'tucuman',
+		'Total infections[a]': 'total_infections',
+		'Total deaths': 'total_deaths',
+		'New cases[a]': 'new_cases',
+		'New deaths': 'new_deaths',
+	};
+
+	return provincesFormatter[province];
+};
+
+const formatDate = date => {
+	const formatMonth = month => {
+		const monthsMapper = {
+			Jan: '01',
+			Feb: '02',
+			Mar: '03',
+			Apr: '04',
+			May: '05',
+			Jun: '06',
+			Aug: '08',
+			Sep: '09',
+			Oct: '10',
+			Nov: '11',
+			Dec: '12',
+		};
+		return monthsMapper[month];
+	};
+
+	const formatDay = day => (day < 10 ? `0${day}` : day);
+	const [day, month] = date.match(/\w+/g);
+	const year = '2020';
+
+	const formattedDay = formatDay(day);
+	const formattedMonth = formatMonth(month);
+
+	return `${year}-${formattedMonth}-${formattedDay}`;
 };
 
 async function main(request: NowRequest, response: NowResponse) {
@@ -44,11 +76,9 @@ async function main(request: NowRequest, response: NowResponse) {
 				const data = x.split('\n');
 
 				//provinces
-				let columns = data
-					.filter(x => !hasNumber(x))
-					.map(c => provincesFormatter[c]);
+				let columns = data.filter(x => !hasNumber(x)).map(formatProvince);
 
-				const rows = data.filter(hasNumber);
+				const rows = data.filter(hasNumber).map(formatDate);
 
 				return [columns, rows];
 			},
@@ -75,6 +105,7 @@ async function main(request: NowRequest, response: NowResponse) {
 		},
 	});
 
+	// creating one array with cells content for each day
 	data.content = R.splitEvery(24, data.content);
 
 	const [provinces, dates] = data.table;
